a02d58948866fa30fda714c2a0761abca2fe154b
BeaOD <owusubea@gmail.com>
2023-01-27 16:25:00 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//import UsersClass from './classcomponent/UsersClass';
+import UsersFunction from './functioncomponent/UsersFunction'
+
+function App() {
+  return (
+    <>
+       {/* <UsersClass/> */}
+      <UsersFunction/>
+    </>
+  );
+}
+
+export default App;
